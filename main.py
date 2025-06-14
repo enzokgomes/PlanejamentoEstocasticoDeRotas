448db2747c55c913b40a4af3b25aed2bc7c6b7ab
Enzo Gomes <enzo.k.gomes@gmail.com>
2025-06-14 17:31:40 -0300

    cenários

--- main.py
+++ main.py
@@ -7,16 +7,32 @@ current_folder_path = os.path.dirname(current_file_path)
 if os.getcwd() != current_folder_path:
     os.chdir(current_folder_path)
 
-scenarios = [
-    Scenario(px=.35, nt=2000, exchange_rate_variation=0, oil_price_variation=0, 
-             port_call_price_variation=0, intermodal_price_variation=0,
-             demand_variation=500, freight_variation=0),
+px = [.35]
+nt = [2000, 2500, 3000, 3500]
+exchange_rate_variation = [0]
+oil_price_variation = [0]
+port_call_price_variation = [0]
+intermodal_price_variation = [0]
 
-    Scenario(px=.35, nt=2000, exchange_rate_variation=0, oil_price_variation=0, 
-             port_call_price_variation=0, intermodal_price_variation=0,
-             demand_variation=550, freight_variation=0),
+num_years = 10
+demand_variation_rate = .1
 
-]
+demand_variation = [(1 + demand_variation_rate) ** year - 1 for year in range(num_years)]
+
+freight_variation = [0]
+
+scenarios = []
+
+for _px in px:
+    for _nt in nt:
+        for _exchange_rate_variation in exchange_rate_variation:
+            for _oil_price_variation in oil_price_variation:
+                for _port_call_price_variation in port_call_price_variation:
+                    for _intermodal_price_variation in intermodal_price_variation:
+                        for _demand_variation in demand_variation:
+                            for _freight_variation in freight_variation:
+                                scenario = Scenario(px=_px, nt=_nt, exchange_rate_variation=_exchange_rate_variation, oil_price_variation=_oil_price_variation, port_call_price_variation=_port_call_price_variation, intermodal_price_variation=_intermodal_price_variation, demand_variation=_demand_variation, freight_variation=_freight_variation)
+                                scenarios.append(scenario)
 
 file_name = f'Input/Dados.xlsx'
 
